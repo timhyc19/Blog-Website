5366ef7686b8172b1be1e3ab21778ed544475ae7
Tim Chung <31941027+timhyc19@users.noreply.github.com>
2021-01-09 17:44:11 -0500

    message

--- app.js
+++ app.js
@@ -88,12 +88,6 @@ const requestedPostId = req.params.postId;
 //  }
 //})
 
-let port = process.env.PORT;
-if (port == null || port == "") {
-  port = 3000;
-}
-app.listen(port);
-
-app.listen(port, function() {
+app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
